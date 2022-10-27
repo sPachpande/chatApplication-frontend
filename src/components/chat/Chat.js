127e4b761ead63ebd0ea0442bd87518e3b6b73f5
Shubham Pachpande <shubhamspachpande@gmail.com>
2022-10-27 14:34:36 +0530

    [Shubham] Add. added message received notification.

--- src/components/chat/Chat.js
+++ src/components/chat/Chat.js
@@ -1,62 +1,81 @@
-import React, { useState,useEffect} from 'react'
+import React, { useState, useEffect } from 'react'
 import Users from '../users/Users'
 import styles from './styles/chatStyles';
 import ChatArea from '../chatArea/ChatArea';
 import socketClient from "socket.io-client";
 import chatService from './services/chatService';
+import Snackbar from '@mui/material/Snackbar';
+import Alert from '@mui/material/Alert';
 
 
 export default function Chat() {
     const [receiver, setReceiver] = useState(null);
-    const [messages,setMessages] = useState([]);
+    const [messages, setMessages] = useState([]);
     const [toggleRefresh, setToggleRefresh] = useState(false)
     const [receiverName, setReceiverName] = useState(null);
     const [socket, setSocket] = useState(null);
-    const [notification,setNotification]  = useState(false);
-    const [notificationSender,setNotificationSender] = useState(null);
+    const [notification, setNotification] = useState(false);
+    const [notificationSender, setNotificationSender] = useState(null);
+    const [notifications,setNotifications] = useState([])
 
     useEffect(() => {
-        if(receiver){
-        chatService.fetchMessages(receiver).then(response => {
-            const fetchedMessages = response.data;
-            if(messages.length!==fetchedMessages.length)
-                setMessages(fetchedMessages);
+        if (receiver) {
+            chatService.fetchMessages(receiver).then(response => {
+                const fetchedMessages = response.data;
+                if (messages.length !== fetchedMessages.length)
+                    setMessages(fetchedMessages);
             });
-      }});
+        }
+    });
 
-      useEffect(() => {
-        const newSocket = socketClient("http://localhost:8085/", { 
-          query: {
-            "room": localStorage.getItem('chatApp_Id')    //listining 
-          }
-        }); 
-        if(newSocket!=null)
-        {
+    useEffect(() => {
+        const newSocket = socketClient("http://localhost:8085/", {
+            query: {
+                "room": localStorage.getItem('chatApp_Id')    //listining 
+            }
+        });
+        if (newSocket != null) {
             setSocket(newSocket);
         }
-        
-      },[]);
 
-      const handleNotification = (notification)=>{
+    }, []);
+
+    const handleNotification = (notification) => {
         setToggleRefresh(prevState => !prevState)
         setNotification(true);
         setNotificationSender(notification.senderId);
-       
+
     }
-      useEffect(()=>{  
-        if(socket)
-        {
-            socket.on("get_notification",handleNotification)
+    useEffect(() => {
+        if (socket) {
+            socket.on("get_notification", handleNotification)
         }
-    },[socket]);
+    }, [socket]);
+
+    const handleClose = () => {
+        setNotification(false)
+    }
 
     const classes = styles();
+
+
     
+
     return (
         <div className={classes.app}>
-            <div className={classes.users}><Users setMessages={setMessages} setReceiver={setReceiver} setReceiverName={setReceiverName} socket={socket}/> </div>
-            <ChatArea setToggleRefresh={setToggleRefresh} messages={messages} receiver={receiver} receiverName={receiverName} socket={socket}/>
+            <div className={classes.users}><Users setMessages={setMessages} setReceiver={setReceiver} setReceiverName={setReceiverName} socket={socket} notifications={notifications} setNotifications={setNotifications} receiver={receiver}/> </div>
+            <ChatArea setToggleRefresh={setToggleRefresh} messages={messages} receiver={receiver} receiverName={receiverName} socket={socket} />
+
+            <Snackbar open={notification} autoHideDuration={2000} onClose={handleClose} sx={{ height: "1%", width: 445 }} anchorOrigin={{
+                vertical: "top",
+                horizontal: "center"
+            }}>
+                <Alert severity="warning" sx={{ width: '100%' }}>
+                    Message received from {notificationSender}
+                </Alert>
+            </Snackbar>
 
         </div>
+
     )
 }
--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -14,35 +14,73 @@ import SenderProfile from '../senderProfile/SenderProfile'
 import chatService from '../chat/services/chatService';
 
 
-function Users({ setMessages, setReceiver, setReceiverName}) {
+function Users({ setMessages, setReceiver, setReceiverName, socket, notifications, setNotifications, receiver }) {
     const classes = styles();
     const { users, usersLoading } = useUsers();
     const [senderName, setSenderName] = useState(null);
     const [senderId, setSenderId] = useState(null);
 
+
+    const handleNotification = (notification) => {
+        setNotifications(prevNotifications => [...prevNotifications, notification.senderId]);
+
+    }
     useEffect(() => {
         setSenderName(localStorage.getItem('chatApp_username'));
         setSenderId(localStorage.getItem('chatApp_Id'));
     }, []);
 
+    useEffect(() => {
+        if (socket) {
+            socket.on("get_notification", handleNotification)
+        }
+    }, [socket]);
+
+
+    const countOccurrences = (val) => {
+        var count = 0;
+
+        for (var i = 0; i < notifications.length; i++) {
+            if (notifications[i] === val)
+                count++;
+        }
+
+        if (receiver && val && receiver.toString().valueOf() === val.valueOf()) {
+            count = 0;
+        }
+
+        return count;
+    };
+
+    const removeOccurrences = (val) => {
+        const updatedNotifications = notifications.filter(i => i !== val);
+        setNotifications(updatedNotifications);
+    };
+
+
 
     faker.seed(23);
     return (
         <div className={classes.users}>
-            <SenderProfile senderName={senderName}/>
+            <SenderProfile senderName={senderName} />
             <List className={classes.listRoot}>
                 {
                     users.map(user => (
                         <div key={user.id} className={classes.usersContainer}>
                             {user.id.toString() === senderId.toString() ? (<></>) :
-                                (<>
+                                (< div style={{display:"flex",justifyContent:"space-between"}}>
                                     <ListItem style={{ height: "70px", cursor: 'pointer' }}
+
                                         onClick={() => {
                                             chatService.fetchMessages(user.id).then(response => {
+
                                                 setReceiver(user.id);
                                                 setReceiverName(user.username);
                                                 setMessages(response.data);
-                                            });
+
+                                                removeOccurrences(user.id.toString());
+                                            })
+
                                         }}>
 
 
@@ -53,21 +91,31 @@ function Users({ setMessages, setReceiver, setReceiverName}) {
                                         </ListItemAvatar>
 
 
-                                        <ListItemText className={classes.itemText} primary={user.username} secondary={
+                                        <ListItemText style={{width:"340px"}}className={classes.itemText} primary={user.username} secondary={
                                             <>
                                                 <Typography
                                                     component="span"
                                                     variant="body2"
+                                                    text-align="right"
                                                     color="textSecondary"
                                                 >
+                                                    {/* {countOccurrences(user.id.toString())} */}
+                                                    {faker.phone.number('+91 #########')}
 
-                                                    {/* {latest Message} */}
+
                                                 </Typography>
                                             </>
                                         } />
+                                       { countOccurrences(user.id.toString()) === 0 ? (<></>) :(<><ListItemText
+                                            style={{
+                                                background: "#25D366",
+                                                borderRadius: "0.8em",
+                                                color: "#ffffff",
+                                                textAlign: "center"
+                                            }} primary={countOccurrences(user.id.toString())} /></>)}
 
                                     </ListItem>
-                                </>)}
+                                </div>)}
                         </div>
                     ))
                 }
